6d51001e7d910c8fac701d311ebacaaf80984f9e
Egor <roso3600@gmail.com>
2023-12-30 18:50:44 +0300

    media text

--- src/styles/Common.tsx
+++ src/styles/Common.tsx
@@ -8,6 +8,17 @@ type FontPropsType = {
     Fmax: number
 }
 
+let variablePart = 0
+const findRem = (Fmax: number) => {
+    variablePart = (Fmax - Fmax/2) / (1220 - 360);
+
+    return parseFloat(((Fmax - 1220 * variablePart) / 16).toFixed(3));
+}
+const findVw = () => {
+    
+    const result = parseFloat((100 * variablePart).toFixed(2));
+    return result;
+}
 export const font = ({family, weight, color, height, Fmax}: FontPropsType) =>
 `
     font-family: ${family || "'Montserrat-Bold', sans-serif"}; 
@@ -15,4 +26,5 @@ export const font = ({family, weight, color, height, Fmax}: FontPropsType) =>
     color: ${color || theme.colors.descFont}; 
     line-height: ${height || 1.2};
     font-size: calc((100vw - 360px)/(1220 - 360) *(${Fmax} - ${Fmax/2}) + ${Fmax/2}px);
+    font-size: clamp(${Fmax/2}px, ${findRem(Fmax)}rem + ${findVw()}vw, ${Fmax}px);
 `
